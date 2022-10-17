5c12474a89d250f5e7cbbfb15dcc910adab3e46b
savagejacke <savagejacke@vt.edu>
2022-10-17 18:28:32 -0400

    Some files not added

---
+++ API/Controllers/NinthController.cs
@@ -0,0 +1,53 @@
+using API.Data;
+using API.DTOs;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NinthController : ControllerBase
+{
+    private readonly DataContext context;
+
+    public NinthController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<NinthResult>>> GetResults()
+    {
+        return await context.NinthResults.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<NinthResult>> GetResults(int id)
+    {
+        var results = await context.NinthResults.ToListAsync();
+        return results.Find(r => r.Id == id);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<NinthResult>> AddResult(NinthDTO dto)
+    {
+        // TODO: Start using automapper
+        var result = new NinthResult
+        {
+            Player1Name = dto.Player1Name,
+            Player1Army = dto.Player1Army,
+            Player1Score = dto.Player1Score,
+            Player2Name = dto.Player2Name,
+            Player2Army = dto.Player2Army,
+            Player2Score = dto.Player2Score,
+            NumberOfRounds = dto.NumberOfRounds,
+            Description = dto.Description
+        };
+        context.NinthResults.Add(result);
+
+        if (await context.SaveChangesAsync() > 0) return result;
+        return BadRequest();
+    }
+}
---
+++ API/DTOs/NinthDTO.cs
@@ -0,0 +1,13 @@
+namespace API.DTOs;
+
+public class NinthDTO
+{
+    public string Player1Name { get; set; }
+    public string Player1Army { get; set; }
+    public int Player1Score { get; set; }
+    public string Player2Name { get; set; }
+    public string Player2Army { get; set; }
+    public int Player2Score { get; set; }
+    public int NumberOfRounds { get; set; }
+    public string Description { get; set; }
+}
